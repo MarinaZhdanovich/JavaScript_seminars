815267a23bbe74d3f036c3ef278ba04501889d01
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2024-07-11 20:24:45 +0300

    lesson1_pro

---
+++ lesson1_pro/script.js
@@ -0,0 +1,8 @@
+const paragrafElement = document.querySelector("#block  p");
+
+const p1 = document.querySelector('p');
+console.log(p1);
+
+console.log(paragrafElement);
+const paragrafElement2 = document.querySelector(".www");
+console.log(paragrafElement2);
