882e299fcb1d4c6e32f54dde78c814786f46c218
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2024-08-01 14:07:35 +0300

    Lesson7

---
+++ lesson7_pro/js/api.js
@@ -0,0 +1,7 @@
+export async function fetchRepos(username) {
+  const response = await fetch(`https://api.github.com/users/${username}/repos`)
+  if (!response.ok) {
+    throw new Error('Пользователь не найден')
+  }
+  return response.json()
+}
